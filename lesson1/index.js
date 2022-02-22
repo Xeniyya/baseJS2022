2c32244cc523625c61c73fdc8ce637693225e43b
Ksenia <ksusha122@yandex.ru>
2022-02-22 22:30:32 +0300

    The small game was added in .js file; Information about the author was added in .md file.

--- lesson1/index.js
+++ lesson1/index.js
@@ -1,2 +1,12 @@
-console.log("HELLO JAVASCRIPT")
-const myName = "Ksenia"
+console.log("HELLO JAVASCRIPT!!!!!");
+const myName = "Ksenia";
+const myBirthdayYear = 1987;
+
+let birtday = prompt("Угадайте мой год рождения!");
+if (birtday < myBirthdayYear) {
+  alert("Я не такая старая!");
+} else if (birtday > myBirthdayYear) {
+  alert("Вы мне льстите!");
+} else {
+  alert("Точно!");
+}
